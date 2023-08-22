c52dc59672b6eb7ae86264637703f3ad02f64504
tauraamui <adampstringer@protonmail.com>
2023-08-22 16:03:30 +0100

    feat: implement matrixes with cell rendering

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"log"
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/f32"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
@@ -24,35 +24,20 @@ const (
 	cellWidth   = 150
 	cellHeight  = 50
 	cellPadding = 4
+	zoomLevel   = 0.6
 )
 
 func (m Matrix) Layout(gtx layout.Context) layout.Dimensions {
-	for i := 0; i < 10; i++ {
-		cell := image.Rect((cellWidth*i)+cellPadding, 0, ((cellWidth * i) + cellWidth), cellHeight)
-		fmt.Printf("CELL: %+v\n", cell)
-		d := clip.Rect{Min: cell.Min, Max: cell.Max}.Push(gtx.Ops)
-		paint.ColorOp{Color: m.Color}.Add(gtx.Ops)
-		paint.PaintOp{}.Add(gtx.Ops)
-		d.Pop()
-	}
-	/*
-		for ri, row := range m.Cells {
-			y := ri
-			for ci := range row {
-				x := 150 * ci
-				cell := image.Rect(x, y, x+150, y+50)
-				fmt.Printf("CELL min: %v, max: %v\n", cell.Min, cell.Max)
-				defer clip.Rect{Min: cell.Min, Max: cell.Max}.Push(gtx.Ops).Pop()
-			}
+	for x, row := range m.Cells {
+		for y := range row {
+			cell := image.Rect((cellWidth*x)+cellPadding, (y*cellHeight)+cellPadding, ((cellWidth * x) + cellWidth), ((cellHeight * y) + cellHeight))
+			cl := clip.Rect{Min: cell.Min, Max: cell.Max}.Push(gtx.Ops)
+			paint.ColorOp{Color: m.Color}.Add(gtx.Ops)
+			paint.PaintOp{}.Add(gtx.Ops)
+			cl.Pop()
 		}
-	*/
-	/*
-		defer clip.Rect{Max: image.Pt(150, 50)}.Push(gtx.Ops).Pop()
-		paint.ColorOp{Color: m.Color}.Add(gtx.Ops)
-		paint.PaintOp{}.Add(gtx.Ops)
-		return layout.Dimensions{Size: image.Pt(150, 50)}
-	*/
-	return layout.Dimensions{Size: image.Pt(0, 200)}
+	}
+	return layout.Dimensions{Size: image.Pt(100, 200)}
 }
 
 func main() {
@@ -70,7 +55,7 @@ func main() {
 func run(w *app.Window) error {
 	m := Matrix{
 		Color: color.NRGBA{R: 0xff, G: 0xff, B: 0xff, A: 255},
-		Cells: [][]int{{0}, {0}, {0}, {0}},
+		Cells: [][]int{{0, 0}, {0, 0}, {0, 0}, {0, 0}},
 	}
 	var ops op.Ops
 	for {
@@ -81,12 +66,16 @@ func run(w *app.Window) error {
 		case system.FrameEvent:
 			gtx := layout.NewContext(&ops, e)
 
+			scale := op.Affine(f32.Affine2D{}.Scale(f32.Point{}, f32.Point{X: zoomLevel, Y: zoomLevel})).Push(gtx.Ops)
+
 			paint.ColorOp{Color: color.NRGBA{R: 0x00, G: 0x00, B: 0x00, A: 255}}.Add(gtx.Ops)
 			paint.PaintOp{}.Add(gtx.Ops)
 
 			m.Layout(gtx)
 
 			e.Frame(gtx.Ops)
+
+			scale.Pop()
 		}
 	}
 }
