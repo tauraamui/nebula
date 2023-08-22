178fb477bc20b39557d6731e8a556c1b38620c99
tauraamui <adampstringer@protonmail.com>
2023-08-22 15:26:51 +0100

    render full row of cells no padding

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"log"
@@ -19,11 +20,39 @@ type Matrix struct {
 	Cells [][]int
 }
 
+const (
+	cellWidth   = 150
+	cellHeight  = 50
+	cellPadding = 1
+)
+
 func (m Matrix) Layout(gtx layout.Context) layout.Dimensions {
-	defer clip.Rect{Max: image.Pt(150, 50)}.Push(gtx.Ops).Pop()
-	paint.ColorOp{Color: m.Color}.Add(gtx.Ops)
-	paint.PaintOp{}.Add(gtx.Ops)
-	return layout.Dimensions{Size: image.Pt(150, 50)}
+	for i := 0; i < 10; i++ {
+		cell := image.Rect(cellWidth*i, 0, ((cellWidth * i) + cellWidth), cellHeight)
+		fmt.Printf("CELL: %+v\n", cell)
+		d := clip.Rect{Min: cell.Min, Max: cell.Max}.Push(gtx.Ops)
+		paint.ColorOp{Color: m.Color}.Add(gtx.Ops)
+		paint.PaintOp{}.Add(gtx.Ops)
+		d.Pop()
+	}
+	/*
+		for ri, row := range m.Cells {
+			y := ri
+			for ci := range row {
+				x := 150 * ci
+				cell := image.Rect(x, y, x+150, y+50)
+				fmt.Printf("CELL min: %v, max: %v\n", cell.Min, cell.Max)
+				defer clip.Rect{Min: cell.Min, Max: cell.Max}.Push(gtx.Ops).Pop()
+			}
+		}
+	*/
+	/*
+		defer clip.Rect{Max: image.Pt(150, 50)}.Push(gtx.Ops).Pop()
+		paint.ColorOp{Color: m.Color}.Add(gtx.Ops)
+		paint.PaintOp{}.Add(gtx.Ops)
+		return layout.Dimensions{Size: image.Pt(150, 50)}
+	*/
+	return layout.Dimensions{Size: image.Pt(0, 200)}
 }
 
 func main() {
@@ -51,6 +80,9 @@ func run(w *app.Window) error {
 			return e.Err
 		case system.FrameEvent:
 			gtx := layout.NewContext(&ops, e)
+
+			paint.ColorOp{Color: color.NRGBA{R: 0xff, G: 0xff, B: 0xff, A: 0xff}}.Add(gtx.Ops)
+			paint.PaintOp{}.Add(gtx.Ops)
 
 			m.Layout(gtx)
 
