45b44d6c45ab8733c80d8d0823aa948704585e99
tauraamui <adampstringer@protonmail.com>
2023-09-09 20:06:19 +0100

    remove tracing

--- main.go
+++ main.go
@@ -3,30 +3,16 @@ package main
 import (
 	"log"
 	"os"
-	"runtime/trace"
 
 	"gioui.org/app"
 	"github.com/tauraamui/nebula/nebula"
 )
 
 func main() {
-	f, err := os.Create("trace.out")
-	if err != nil {
-		log.Fatalf("failed to create trace output file: %v", err)
-	}
-
-	if err := trace.Start(f); err != nil {
-		log.Fatalf("failed to start trace: %v", err)
-	}
-
 	appx := nebula.New()
 	go func() {
 		if err := appx.Run(); err != nil {
 			log.Fatal(err)
-		}
-		trace.Stop()
-		if err := f.Close(); err != nil {
-			log.Fatalf("failed to close trace file: %v", err)
 		}
 		os.Exit(0)
 	}()
